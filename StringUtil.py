66f8080d530968bcb4146f10903aaac23a99090a
Aparecida Silva <cidinhalilisilva01@gmail.com>
2016-10-03 23:45:23 -0300

    StringUtil.py

---
+++ StringUtil.py
@@ -0,0 +1,17 @@
+def output(texto):
+    print(texto)
+
+def tamanho(texto):
+    tamanho = len(texto)
+    print ("Tamanho do Texto: ", tamanho)
+    return tamanho
+
+def maiusculo(texto):
+    maiusculo = texto.upper()
+    print ("Maiúsculo: ", maiusculo)
+    return maiusculo
+
+def minusculo(texto):
+    minusculo = texto.lower
+    print ("Minúsculo: ", minusculo)
+    return minusculo
